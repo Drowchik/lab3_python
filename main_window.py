4dba0e47aba7703f615ffc988ba10317f2ecaf23
natal <natal@CLOWN>
2023-10-29 13:39:44 +0400

    fixed_and_add_rating

--- main_window.py
+++ main_window.py
@@ -2,10 +2,26 @@ import sys
 import os
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QFont, QIcon
+from PyQt5.QtCore import Qt
 from task_1 import run1
 import task_2 
 import task_3
+import task_5
 
+class ScrollLabel(QScrollArea):
+    def __init__(self, *args, **kwargs):
+        QScrollArea.__init__(self, *args, **kwargs)
+        self.setWidgetResizable(True)
+        text = QWidget(self)
+        self.setWidget(text)
+        lay = QVBoxLayout(text)
+        self.label = QLabel(text)
+        self.label.setWordWrap(True)
+        lay.addWidget(self.label)
+
+    def setText(self, text):
+        self.label.setText(text)
+        
 class Example(QWidget):
 
     def __init__(self):
@@ -15,31 +31,60 @@ class Example(QWidget):
             "background:#fec7d7; color: #fffffe; font-weight:bold; border-radius: 5px;")
 
     def initUI(self):
+        
         self.setWindowTitle('6212 - lab_3')
         QToolTip.setFont(QFont('SansSerif', 10))
         self.setWindowTitle('6212 - lab3')
         self.setWindowIcon(QIcon('home.png'))
         
         self.button = QPushButton('Create CSV-Dataset', self)
-        self.button.move(50, 50)
         self.button.adjustSize()
         self.button.clicked.connect(self.click_csv)
-        self.button.setStyleSheet("background:#0e172c; border-radius: 5px; min-width: 300px;")
+        self.button.setStyleSheet("background:#0e172c; border-radius: 5px; min-width: 300px; min-height: 100px;")
         
         self.button_dataset = QPushButton('Create Dataset-Copy', self)
-        self.button_dataset.move(450, 50)
         self.button_dataset.adjustSize()
         self.button_dataset.clicked.connect(self.click_dataset)
-        self.button_dataset.setStyleSheet("background:#0e172c; border-radius: 5px; min-width: 300px;")
+        self.button_dataset.setStyleSheet("background:#0e172c; border-radius: 5px; min-width: 300px; min-height: 100px;")
         
         
         self.button_dataset2 = QPushButton('Create Dataset-Copy2', self)
-        self.button_dataset2.move(850, 50)
         self.button_dataset2.adjustSize()
-        self.button_dataset2.clicked.connect(self.click_dataset)
-        self.button_dataset2.setStyleSheet("background:#0e172c; border-radius: 5px; min-width: 300px;")
+        self.button_dataset2.clicked.connect(self.click_dataset_copy)
+        self.button_dataset2.setStyleSheet("background:#0e172c; border-radius: 5px; min-width: 300px; min-height: 100px;")
         
-        self.setGeometry(300, 300, 1200, 200)
+        self.button_rating1 = QPushButton('1', self)
+        self.button_rating1.setStyleSheet("background:#0e172c; border-radius: 5px; min-width: 150px; min-height: 70px;")
+
+        self.button_rating2 = QPushButton('2', self)
+        self.button_rating2.setStyleSheet("background:#0e172c; border-radius: 5px; min-width: 150px; min-height: 70px;")
+        self.button_rating3 = QPushButton('3', self)
+        self.button_rating3.setStyleSheet("background:#0e172c; border-radius: 5px; min-width: 150px; min-height: 70px;")
+        self.button_rating4 = QPushButton('4', self)
+        self.button_rating4.setStyleSheet("background:#0e172c; border-radius: 5px; min-width: 150px; min-height: 70px;")
+        self.button_rating5 = QPushButton('5', self)
+        self.button_rating5.setStyleSheet("background:#0e172c; border-radius: 5px; min-width: 150px; min-height: 70px;")
+        
+        
+        self.review_label = ScrollLabel(self)
+        self.review_label.setStyleSheet("background:#d9d4e7;  border: 5px solid #0e172c")
+        
+        
+        layout = QGridLayout()
+        self.setLayout(layout)
+        
+        layout.addWidget(self.button, 0, 0)
+        layout.addWidget(self.button_dataset, 0, 1, 1, 3)
+        layout.addWidget(self.button_dataset2, 0, 4)
+        layout.addWidget(self.review_label, 1, 0, 3, 5)
+        
+        layout.addWidget(self.button_rating1, 5, 0)
+        layout.addWidget(self.button_rating2, 5, 1)
+        layout.addWidget(self.button_rating3, 5, 2)
+        layout.addWidget(self.button_rating4, 5, 3)
+        layout.addWidget(self.button_rating5, 5, 4)
+        
+        # self.setGeometry(300, 300, 1200, 200)
         self.show()
 
     def click_csv(self):
@@ -60,7 +105,7 @@ class Example(QWidget):
         
         create_button = QPushButton("Сreate CSV", dialog)
         create_button.setStyleSheet("background:#0e172c; border-radius: 5px;")
-        create_button.clicked.connect(self.click_create_csv)
+        create_button.clicked.connect(self.click_create_csv1)
         create_button.adjustSize()
         
         
@@ -82,6 +127,7 @@ class Example(QWidget):
         task_2.copy_info(self.folderpath)
     
     def create_dataset_copy2(self):
+        print('asdas')
         task_3.run3(self.folderpath)
         
     def click_dataset(self):
@@ -107,7 +153,7 @@ class Example(QWidget):
         
         create_button = QPushButton("Сreate CSV", dialog)
         create_button.setStyleSheet("background:#0e172c; border-radius: 5px;")
-        create_button.clicked.connect(self.click_create_csv)
+        create_button.clicked.connect(self.click_create_csv2)
         create_button.adjustSize()
         
         
@@ -145,7 +191,7 @@ class Example(QWidget):
         
         create_button = QPushButton("Сreate CSV", dialog)
         create_button.setStyleSheet("background:#0e172c; border-radius: 5px;")
-        create_button.clicked.connect(self.click_create_csv)
+        create_button.clicked.connect(self.click_create_csv2)
         create_button.adjustSize()
         
         
@@ -167,7 +213,7 @@ class Example(QWidget):
             
     def click_create_csv2(self):
         if 'dataset' in self.folderpath:
-            task_2.run2(self.folderpath)
+            task_2.run2()
         else:
             ErrorMessageBox(self, "Please choose directory with dataset")
             
@@ -186,7 +232,7 @@ class ErrorMessageBox:
 def application(): 
     app = QApplication(sys.argv)
     ex = Example()
-
+    ex.show()
     sys.exit(app.exec_())
     
     
